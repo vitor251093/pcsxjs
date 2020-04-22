76dd84ffcec459f5ada7e3b178c94f11d695f412
vitor.miranda <vitor.miranda@virtualinteractions.com.br>
2020-04-22 20:29:41 -0300

    Starting the PS4 support

--- pcsx_ui.js
+++ pcsx_ui.js
@@ -5,6 +5,7 @@ var do_iter = true;
 var Module = {
   preRun: [],
   postRun: [],
+  wasmJSMethod: "asmjs",
   print: (function () {
     return function (text) {
       if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
